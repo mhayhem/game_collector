7b3b5685052e7e081b217c3aa09fca03bfaf3a23
MhayhemDev <mhayhemdev@gmail.com>
2026-06-19 00:21:48 +0100

    added class user

--- app/models.py
+++ app/models.py
@@ -1,10 +1,17 @@
 class Game:
-    def __init__(self, title: str, genre: str, platform: str, game_format: str, status: str):
+    def __init__(self, title: str, genre: str, platform: str, game_format: str, status: str) -> None:
         self.title = title
         self.genre = genre
         self.platform = platform
         self.game_format = game_format
         self.status = status
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.title} ({self.platform}) {self.status}"
+
+
+class User:
+    def __init__(self, username: str, password: str) -> None:
+        self.username = username
+        self.password = password
+
