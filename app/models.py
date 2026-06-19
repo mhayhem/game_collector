23051784d1af233910c5b857e37c70dca57ac988
MhayhemDev <mhayhemdev@gmail.com>
2026-06-20 00:13:04 +0100

    added any files

--- app/models.py
+++ app/models.py
@@ -1,17 +1,23 @@
 class Game:
-    def __init__(self, title: str, genre: str, platform: str, game_format: str, status: str) -> None:
+    def __init__(
+            self, title: str, genre: str, platform: str, game_format: str, 
+            status: str, img_url: str, game_id: int | None = None, user_id: int | None = None) -> None:
+        
+        self.game_id = game_id
         self.title = title
         self.genre = genre
         self.platform = platform
         self.game_format = game_format
         self.status = status
+        self.img_url = img_url
     
     def __str__(self) -> str:
         return f"{self.title} ({self.platform}) {self.status}"
 
 
 class User:
-    def __init__(self, username: str, password: str) -> None:
+    def __init__(self, username: str, password_hash: str, user_id: int | None = None) -> None:
+        self.user_id = user_id
         self.username = username
-        self.password = password
+        self.password_hash = password_hash
 
