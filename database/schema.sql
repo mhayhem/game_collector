51d5d33a610a5ffc8222d82c83b29ce2c4505a84
MhayhemDev <mhayhemdev@gmail.com>
2026-06-24 00:53:53 +0100

    add init_db.py

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,17 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXIST users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXIST games (
+    game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL
+    genre TEXT NOT NULL,
+    platform TEXT NOT NULL,
+    game_format TEXT NOT NULL,
+    status TEXT NOT NULL,
+    img_url TEXT NOT NULL DEFAULT "No image",
+    user_id INTEGER NOT NULL,
 );
