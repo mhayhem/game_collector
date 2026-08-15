7763024c696acacd2aebb2b921aecf6c2a98073c
MhayhemDev <mhayhemdev@gmail.com>
2026-08-15 17:28:17 +0100

    remove database files

--- database/init_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("database/game_collector.db")
-
-with open("database/schema.sql") as file:
-    connection.executescript(file.read())
-    
-connection.close()
