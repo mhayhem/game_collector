0e8371fd857b6218f5c87095dc5149919317a366
MhayhemDev <mhayhemdev@gmail.com>
2026-08-18 00:22:10 +0100

    Refactorin files sql

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -1,8 +1,22 @@
-import sqlite3
+from pathlib import Path
+from app.database.db_script import get_connection
 
-connection = sqlite3.connect("database/game_collector.db")
+DATABASE_DIR = Path(__file__).resolve().parent
 
-with open("database/schema.sql") as file:
-    connection.executescript(file.read())
+SCHEMA_DIR = DATABASE_DIR / "schema.sql"
+SEEDS_DIR = DATABASE_DIR / "seeds.sql"
+
+CON = get_connection()
+
+def init_db() -> None:
+    try:
+        with open(SCHEMA_DIR, "r", encoding="utf-8") as file:
+            CON.executescript(file.read())
+        
+        with open(SEEDS_DIR, "r", encoding= "utf-8") as file:
+            CON.executescript(file.read())
+        
+        CON.commit()
     
-connection.close()
+    finally:
+        CON.close()
--- app/database/schema.sql
+++ app/database/schema.sql
@@ -1,17 +1,58 @@
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXIST games (
+CREATE TABLE IF NOT EXISTS games (
     game_id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     genre TEXT NOT NULL,
-    platform TEXT NOT NULL,
-    game_format TEXT NOT NULL,
-    status TEXT NOT NULL,
-    img_url TEXT NOT NULL DEFAULT "No image",
-    user_id INTEGER NOT NULL
+    developer TEXT DEFAULT 'Unknow',
+    year_release DATETIME,
+    img_url TEXT NOT NULL DEFAULT 'No image',
 );
+
+CREATE TABLE IF NOT EXISTS manufacturers (
+    manufacturer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS platforms (
+    platform_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    platform_name TEXT NOT NULL UNIQUE,
+    manufacturer_id INTEGER NOT NULL
+
+    FOREIGN KEY (manufacturer_id)
+        REFERENCES manufacturers(manufacturer_id)
+);
+
+CREATE TABLE IF NOT EXISTS user_games (
+    user_game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    game_platform_id INTEGER NOT NULL,
+    game_format TEXT NOT NULL,
+    purchase_status TEXT NOT NULL,
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+    
+    FOREIGN KEY (game_platform_id)
+        REFERENCES game_platform(game_platform_id)
+);
+
+CREATE TABLE IF NOT EXISTS game_platform (
+    game_platform_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER NOT NULL,
+    platform_id INTEGER NOT NULL,
+
+    UNIQUE(game_id, platform_id)
+
+    FOREIGN KEY (game_id)
+        REFERENCES games(game_id)
+    
+    FOREIGN KEY (platform_id)
+        REFERENCES platforms(platform_id)
+);
+
